cbaa978dfcd67a1eb154fd1df472c6f4203cbb67
Owen Adams <owenadams.main@gmail.com>
2022-06-14 02:46:13 +0000

    add ajax call, update event listener. feature 1 complete. #1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 const searchInput = document.querySelector('.search-input');
 const keywordSearch = document.querySelector('#keyword-search');
 
-const searchResult = null;
+const searchResult = [];
 
 keywordSearch.addEventListener('click', event => {
   event.preventDefault();
-  window.location.hash = '#' + searchInput.value;
+  const convertedString = convertSearchString(searchInput.value);
+  window.location.hash = '#search=' + convertedString;
+  searchRecipes(convertedString);
   searchInput.value = '';
 });
 
-// eslint-disable-next-line no-console
-console.log(searchResult);
+function searchRecipes(ingredients) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.spoonacular.com/recipes/findByIngredients?ingredients=' +
+   ingredients +
+   '&apiKey=633237cc8f324710afa989c4ba9993f0&includeNutrition=true&number=10&diet=vegan');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', () => {
+    for (let i = 0; i < xhr.response.length; i++) {
+      searchResult.push(xhr.response[i]);
+    }
+  });
+  xhr.send();
+}
+
+function convertSearchString(string) {
+  const searchString = string.split(' ').join('+');
+  return searchString;
+}
